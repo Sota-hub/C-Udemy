63c9d7d78c74f36041aaeb101d5811080a611222
SOTA <sota00@outlook.jp>
2022-02-16 16:39:43 -0800

    add bit operators

--- helloworld/helloworld.c
+++ helloworld/helloworld.c
@@ -3,15 +3,7 @@
 
 
 
-int main(int argc, char *argv[])
+int main()
 {
-  int numberOfArguments = argc;
-  char *arg1 = argv[0];
-  char *arg2 = argv[1];
-
-  printf("%d\n", numberOfArguments);
-  printf("1 %s\n", arg1);
-  printf("2 %s\n", arg2);
-
   return 0;
 }
