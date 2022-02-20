b9fb6750747170b1e8854eef3895b3bff03a3129
SOTA <sota00@outlook.jp>
2022-02-19 17:26:11 -0800

    reflect pointer

--- challenges/counting.c
+++ challenges/counting.c
@@ -5,16 +5,21 @@ int stringLength(const char *string);
 
 int main()
 {
-  printf("%d", stringLength("TEST"));
+  printf("%d", stringLength("TEST")); 
   return 0;
 }
 
-int stringLength(const char *string) 
+int stringLength(const char *string) //アドレス渡し（TEST の頭文字 T だけ渡す）
 {
-  const char *address = string;
+  const char *address = string;  //ポインタ変数にアドレス代入
+  printf("*address: %d\n", *address); //’T’の値（ASCII = 10進数->84 16進数->54）
+  
+  while(*address) 
+    ++address; // address 1 -> T, 2-> E, 3-> S...
 
-  while(*address)
-    ++address;
-
-  return address - string;
+  return address - string; // ex.2000 - 1996
 }
+
+// *value = アドレス(int func(const char *value))
+// *value = 実際の値(destructuring)
+// value = アドレス(plain)
